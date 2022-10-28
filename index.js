78e8f2ec2edbea681c3794940c1c563ea02b793c
Monciaux Sylvain <monciauxsylvain63@gmail.com>
2022-10-28 15:50:33 +0100

    save project

--- index.js
+++ index.js
@@ -1,8 +1,5 @@
 const main = document.querySelector("main");
-
-//variable stockant tous les exercices
-
-let exerciceArray = [
+const basicAray = [
   { pic: 0, min: 1 },
   { pic: 1, min: 1 },
   { pic: 2, min: 1 },
@@ -15,6 +12,17 @@ let exerciceArray = [
   { pic: 9, min: 1 },
 ];
 
+//variable stockant tous les exercices
+
+let exerciceArray = [];
+
+//get stored exercices Array
+
+(() => {
+  if (localStorage.exercices) {
+  }
+})();
+
 //Generateur d'exercice (création de la class qui va être instancié pour lancer la routine)
 
 class exercice {}
@@ -26,6 +34,52 @@ const utils = {
     document.querySelector("h1").innerHTML = title;
     main.innerHTML = content;
     document.querySelector(".btn-container").innerHTML = btn;
+  },
+  handleEventMinutes: function () {
+    document.querySelectorAll('input[type="number"]').forEach((input) => {
+      input.addEventListener("input", (e) => {
+        exerciceArray.map((exo) => {
+          if (exo.pic == e.target.id) {
+            exo.min = parseInt(e.target.value);
+            console.log(exerciceArray);
+          }
+        });
+      });
+    });
+  },
+  handleEventArrow: function () {
+    document.querySelectorAll(".arrow").forEach((arrow) => {
+      arrow.addEventListener("click", (e) => {
+        let position = 0;
+        exerciceArray.map((exo) => {
+          if (exo.pic == e.target.dataset.pic && position !== 0) {
+            //intervertir
+            [exerciceArray[position], exerciceArray[position - 1]] = [
+              exerciceArray[position - 1],
+              exerciceArray[position],
+            ];
+            page.lobby();
+          } else {
+            position++;
+          }
+        });
+      });
+    });
+  },
+  deleteItem: function () {
+    document.querySelectorAll(".deleteBtn").forEach((btn) => {
+      btn.addEventListener("click", (e) => {
+        const newArr = exerciceArray.filter(
+          (exo) => exo.pic != e.target.dataset.pic
+        );
+        exerciceArray = newArr;
+        page.lobby();
+      });
+    });
+  },
+  reboot: function () {
+    exerciceArray = basicAray;
+    page.lobby();
   },
 };
 
@@ -56,6 +110,12 @@ const page = {
       "<ul>" + mapArray + "</ul>",
       "<button id='start'>Commencer<i class='fa-solid fa-circle-play'></i></button>"
     );
+    //fonction pour ajouter les minutes sur les cards
+    utils.handleEventMinutes();
+    //Fonction pour intervertir les cards
+    utils.handleEventArrow();
+    utils.deleteItem();
+    reboot.addEventListener("click", () => utils.reboot());
   },
 
   routine: function () {
